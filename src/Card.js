7e5a2afc5e14109fcc07c9eaa756f55ebe28e7b6
MJBackes <mjohnbackes@gmail.com>
2020-01-16 13:13:20 -0600

    Switched the Card component back to a class Component since the point of this assignment is to get used to using class components.

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import './Card.css';
-function Card(props){
-        const content = props.wordIsShowing ? props.word : props.definition;
+class Card extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            word: props.word,
+            definition: props.definition,
+            wordIsShowing: props.wordIsShowing,
+            onClick: props.onClick
+        };
+    }
+    componentWillReceiveProps(props){
+        this.setState({
+            word: props.word,
+            definition: props.definition,
+            wordIsShowing: props.wordIsShowing
+        })
+    }
+    componentDidUpdate(props){
+        console.log(this.state.word + " " + this.state.definition);
+    }
+
+    render(){
+        const content = this.state.wordIsShowing ? this.state.word : this.state.definition;
         return (
-            <div onClick={props.onClick}>
+            <div onClick = {this.state.onClick}>
                 <h3>
                     {content}
                 </h3>
             </div>
         );
+    }
 }
 
 export default Card;
